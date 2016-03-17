ee87e53937664707ad1d7a32d91bc7a5e0f67689
JackMasterson <jackmasterson5@gmail.com>
2016-03-17 16:38:23 -0400

    iteratingMarkers
    markers now iterate through locations, and have the ability to click on
    a location at the bottom and it will change and give info about it
    (just the address for now)

--- js/script.js
+++ js/script.js
@@ -1,27 +1,4 @@
 
-
-
-
-      function initMap() {
-      	var mapDiv = document.getElementById('map');
-
-      	
-        var map = new google.maps.Map(mapDiv, {
-          center: {lat: 40.220391, lng: -74.012082},
-          scrollwheel: false,
-          zoom: 14
-        });
-
-
-  var marker = new google.maps.Marker({
-    			position: {lat: 40.216147, lng: -74.012914},
-			title: 'Johnny Mac House of Spirits',
-			map: map,
-  });
-
-
-    };
-
 var	initialPlaces =  [
 		 {
 			position: {lat: 40.216147, lng: -74.012914},
@@ -33,31 +10,81 @@ var	initialPlaces =  [
 			position: {lat: 40.220001, lng: -74.000947},
 			title: 'The Stone Pony',
 			map: map,
-			address: ['913 Ocean Ave, Asbury Park, NJ 07712']
+			address: '913 Ocean Ave, Asbury Park, NJ 07712'
 		}
 	];
 
+var map;
 
-	
-	
+function initMap() {
+      	var mapDiv = document.getElementById('map');
 
+      	
+        var map = new google.maps.Map(mapDiv, {
+          center: {lat: 40.220391, lng: -74.012082},
+          scrollwheel: false,
+          zoom: 14
+        });
+
+
+	for(i=0;i<initialPlaces.length;i++){
+
+		var place = initialPlaces[i];
+
+
+		var marker = new google.maps.Marker({
+			position: place.position,
+			title: place.title,
+			map: map,
+			address: place.address
+		});
+};
+
+
+/*
+  var marker = new google.maps.Marker({
+    		position: {lat: 40.216147, lng: -74.012914},
+			title: 'Johnny Mac House of Spirits',
+			map: map,
+  });
+
+    var marker = new google.maps.Marker({
+			position: {lat: 40.220001, lng: -74.000947},
+			title: 'The Stone Pony',
+			map: map,
+			address: '913 Ocean Ave, Asbury Park, NJ 07712'
+		});
+   
+*/
+    };
+
+
+
+
+
+
+
+/*var len = initialPlaces.length;
+for(i=0;i<len;i++){
+	
+}
+	    var marker = new google.maps.Marker({
+			position: {lat: 40.220001, lng: -74.000947},
+			title: 'The Stone Pony',
+			map: map,
+			address: '913 Ocean Ave, Asbury Park, NJ 07712'
+		});
+	
+*/
 var Place = function(data) {
 	this.position = ko.observable(data.position);
 	this.title = ko.observable(data.title);
-	this.map = ko.observableArray(data.map);
-	this.address = ko.observableArray(data.address);
+	this.map = ko.observable(data.map);
+	this.address = ko.observable(data.address);
 
-		this.title = ko.computed(function(){
-		var title;
-		var clicks = this.clickCount();
-		if(clicks<10) {
-			title = 'Newborn';
-		} else if (clicks<50) {
-			title = 'Infant';
-		}
-		return title;
-	}, this);
 };
+
+
 
 var ViewModel = function() {
 
@@ -72,12 +99,6 @@ var ViewModel = function() {
 	this.currentPlace = ko.observable( this.placeList()[0] );
 
 
-
-
-
-
-
-
 	this.setPlace = function(clickedPlace) {
 		self.currentPlace(clickedPlace);
 	};
@@ -88,7 +109,11 @@ ko.applyBindings(new ViewModel());
 
 
 
-
+ /* var marker2 = new google.maps.Marker({
+    		position: this.position,
+			title: this.title,
+			map: this.map,
+  });*/
 
 
 
