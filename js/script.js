8cae472a42bf566d9c1fd6b8f18f6aa16c3747a9
JackMasterson <jackmasterson5@gmail.com>
2016-03-19 18:58:24 -0400

    cleanedUp
    just took out some spaces in between the working code

--- js/script.js
+++ js/script.js
@@ -5,35 +5,40 @@ var	initialPlaces =  [
 			title: 'Johnny Mac House of Spirits',
 			address: '208 Main St, Asbury Park, NJ 07712',
 			src: 'img/macs.jpg',
-			nums: 1
+			nums: 1,
+			icon: null
 
 		}, {
 			position: {lat: 40.220001, lng: -74.000947},
 			title: 'The Stone Pony',
 			address: '913 Ocean Ave, Asbury Park, NJ 07712',
 			src: 'img/pony.jpg',
-			nums: 2
+			nums: 2,
+			icon: null
 
 		}, {
 			position: {lat: 40.220239, lng: -74.002344},
 			title: 'Porta Pizza/Wine Bar',
 			address: '911 Kingsley St, Asbury Park, NJ 07712',
 			src: 'img/porta.jpg',
-			nums: 3
+			nums: 3,
+			icon: null
 
 		}, {
 			position: {lat: 40.2207, lng: -73.999884},
 			title: 'Silverball Museum',
 			address: '1000 Ocean Ave, Asbury Park, NJ 07712',
 			src: 'img/silverball.jpg',
-			nums: 4
+			nums: 4,
+			icon: null
 
 		}, {
 			position: {lat: 40.223796, lng: -73.998585},
 			title: 'Convention Hall',
 			address: '1300 Ocean Ave, Asbury Park, NJ 07712',
 			src: 'img/hall.jpg',
-			nums: 5
+			nums: 5,
+			icon: null
 
 		}
 	];
@@ -55,65 +60,71 @@ function initMap() {
 	var Marker = function(data) {
 
 		function drop() {
-
 		    addMarkerWithTimeout(data.position, data.nums * 300);
-		  
 		}
 		drop();
-		
 
-	function addMarkerWithTimeout(position, timeout) {
-  		window.setTimeout(function() {
-		var marker = new google.maps.Marker({
-			position: data.position,
-			title: data.title,
-			address: data.address,
-			src: data.src,
-			map: map,
-			num: data.num,
-			animation: google.maps.Animation.DROP
-		});
+		var image = 'img/marker-blue.png';
 
 
 
-
-	
-	
-
-	 var contentString = 
-	 '<div id="content">'+
-      	'<h4>'+marker.title+'</h4>'+
-      	'<h5>'+marker.address+'</h5>'+
-      	'<img class="markerImg" src='+marker.src+'>'+
-     '</div>';
-		
-     var image = 'img/marker-blue.png';
-	  var infowindow = new google.maps.InfoWindow({
-	    content: contentString
-	  });
+		function addMarkerWithTimeout(position, timeout) {
+	  		window.setTimeout(function() {
+			var marker = new google.maps.Marker({
+				position: data.position,
+				title: data.title,
+				address: data.address,
+				src: data.src,
+				map: map,
+				num: data.num,
+				icon: null,
+				animation: google.maps.Animation.DROP
+			});
 
 
-	    marker.addListener('click', function() {
-	    	infowindow.open(map, marker);
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-			timeoutID = window.setTimeout(stopBouncing, 2200);
-				function stopBouncing() {
-				marker.setAnimation(null);
-	  		};
-	  		if(marker.icon == image) {
-	  			marker.setIcon(null);
-	  			infowindow.close(map, marker);
-	  			marker.setAnimation(null);
-	  		}
-	  		else{
-	  			marker.setIcon(image);
-	  		}
+			 var contentString = 
+			 '<div id="content">'+
+		      	'<h4>'+marker.title+'</h4>'+
+		      	'<h5>'+marker.address+'</h5>'+
+		      	'<img class="markerImg" src='+marker.src+'>'+
+		     '</div>';
+				
+		     var image = 'img/marker-blue.png';
+			  var infowindow = new google.maps.InfoWindow({
+			    content: contentString
+			  });
+
+
+		    marker.addListener('click', function() {
+		    	infowindow.open(map, marker);
+				marker.setAnimation(google.maps.Animation.BOUNCE);
+				timeoutID = window.setTimeout(stopBouncing, 2200);
+					function stopBouncing() {
+					marker.setAnimation(null);
+		  		};
+
+
+		  		if(marker.icon == image) {
+		  			marker.setIcon(null);
+		  			infowindow.close(map, marker);
+		  			marker.setAnimation(null);
+
+		  		}
+		  		else{
+		  			marker.setIcon(image);
+
+		  		}
+			  	console.log(marker.title);
+			  	console.log(marker.icon);
+			});
+			console.log(marker.icon);
 
 
 
-	  	console.log(marker.title);
-		});
+
+
 	  	}, timeout);
+
 	}
 
 }
@@ -138,24 +149,15 @@ function initMap() {
 		});
 		this.currentPlace = ko.observable( this.placeList()[0] );
 
-
+			
 
 		this.markerList = ko.observableArray([]);
 		initialPlaces.forEach(function(markerItem){
 			self.markerList.push( new Marker(markerItem) );
 		});
 
+
 		this.currentMarker = ko.observable( this.markerList()[0] );
-
-
-
-
-
-	//    setTimeout(drop, data.num * 200);
-
-
-
-	
 
 		this.setPlace = function(clickedPlace) {
 			self.currentPlace(clickedPlace);
@@ -163,8 +165,8 @@ function initMap() {
 		};	
 
 
-
 	};
+
 	ko.applyBindings(new ViewModel());
 };
 
