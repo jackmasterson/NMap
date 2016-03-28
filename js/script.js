3fcb30b946a8f3a3e0528129dd3d1b60ff10092a
JackMasterson <jackmasterson5@gmail.com>
2016-03-28 00:31:25 -0400

    navBar
    navBar moved from left-handed to up top, display now inline-block
    instead of one on top of the other

--- js/script.js
+++ js/script.js
@@ -156,7 +156,7 @@ function initMap() {
 			icon: null,
 			marker: marker = []
 		}
-		
+		      
 	];
 
 //console.log(initialPlaces[0].marker);
@@ -350,34 +350,6 @@ Searched();
 
 //census api 
 
-var key = "7e7eaf7f8c2f106b19718825f5daf1aa8898aaae";
-var youtubeURL =  "https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?" +
-        "part=snippet" +
-        "&order=10" +
-        "&q=skateboarding+dog" +
-        "&type=video" +
-        "&videoDefinition=high";
-
-
-
-// After the API loads, call a function to enable the search box.
-function handleAPILoaded() {
-  $('#search-button').attr('disabled', false);
-}
-
-// Search for a specified string.
-function search() {
-  var q = $('#query').val();
-  var request = gapi.client.youtube.search.list({
-    q: q,
-    part: 'snippet'
-  });
-
-  request.execute(function(response) {
-    var str = JSON.stringify(response.result);
-    $('#search-container').html('<pre>' + str + '</pre>');
-  });
-}
 
 
 
